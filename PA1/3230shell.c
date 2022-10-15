919fad5e1075d485225255558414ecea02cc9151
JOEYLEE1121 <joeylee1121@gmail.com>
2022-10-15 15:28:37 +0000

    pipe init

--- PA1/3230shell.c
+++ PA1/3230shell.c
@@ -7,19 +7,30 @@
 #include <signal.h>
 #include <stddef.h>
 
-#define MAX_SIZE_CMD 256
-#define MAX_SIZE_ARG 16
+#define MAX_SIZE_CMD 8192
+#define MAX_SIZE_ARG 8192
 
 char cmd[MAX_SIZE_CMD];
+
 char *argv[MAX_SIZE_ARG];
+char *argv2[MAX_SIZE_ARG];
+char *argv3[MAX_SIZE_ARG];
+char *argv4[MAX_SIZE_ARG];
+char *argv5[MAX_SIZE_ARG];
+
 pid_t pid;
-char i;
+
+int i;
+int sizeofargv;
 
 void block_sig();
 void get_cmd();
 void convert_cmd();
 void timeX();
 int exitfunc();
+int check_pipe();
+void without_pipe();
+void with_pipe();
 
 int main(){
 
@@ -31,9 +42,7 @@ int main(){
             continue;
         }
 
-        // printf("cmd before convert: %s\n", cmd);
         convert_cmd();
-        // printf("cmd after convert: %s\n", cmd);
 
         if(!strcmp(cmd, "exit")) {
             if (exitfunc() == 1) {
@@ -42,16 +51,12 @@ int main(){
             else continue;
         }
 
-        pid = fork();
-        if(pid < 0){
-            fprintf(stderr, "fork() Failed");
-            exit(1);
-        } else if (pid == 0) {
-            execvp(argv[0], argv);
-            printf("3230shell: '%s': No such file or directory\n", argv[0]);
-            exit(1);
+        check_pipe();
+
+        if (check_pipe() == 0) {
+            without_pipe();
         } else {
-            if(NULL == argv[i]) waitpid(pid, NULL, 0);
+            with_pipe();
         }
     }
     return 0;
@@ -59,28 +64,27 @@ int main(){
 
 void block_sig(){
     sigset_t block_set;
-
     sigemptyset(&block_set);
-
     sigaddset(&block_set, SIGINT);
-
     sigprocmask(SIG_BLOCK, &block_set, NULL);
 }
 
 void timeX(){
-    // add exec logic here
+    // add logic here
 }
 
 int exitfunc(){
     if (argv[1] != NULL) {
         printf("3230shell: \"exit\" with other arguments!!!\n");
         return 0;
-    } else {
-        printf("3230shell: Terminated");
-        // realase all resources and terminate. show the 
+    } else { 
+        printf("3230shell: Terminated\n");
+        // release all the data and terminate?
+        char *terminate[2] = {"ps", NULL};
+        execv("/bin/ps", terminate);
+
         return 1;
     }
-    // add exit logic here
 }
 
 void get_cmd(){
@@ -95,10 +99,12 @@ void convert_cmd(){
 
     char *ptr;
     i = 0;
+    sizeofargv = 0;
     ptr = strtok(cmd, " ");
     while(ptr != NULL){
         argv[i] = ptr;
         i++;
+        sizeofargv++;
         ptr = strtok(NULL, " ");
     }
 
@@ -108,7 +114,63 @@ void convert_cmd(){
 	    } else{
 	        argv[i] = NULL;
 	}
+}
+    //debug argv
+    /*
+    printf("argv: ");
+    for (int i; i<MAX_SIZE_ARG; i++) {
+        printf("argv[%d]: %s ", i, argv[i]);
+    }
+    printf("\n");
+    printf("argv2: ");
+    for (int i; i<MAX_SIZE_ARG; i++) {
+        printf("argv2[%d]: %s ", i, argv2[i]);
+    }
+    printf("\n");
+    printf("argv3: ");
+    for (int i; i<MAX_SIZE_ARG; i++) {
+        printf("argv3[%d]: %s ", i, argv3[i]);
+    }
+    printf("\n");
+    printf("argv4: ");
+    for (int i; i<MAX_SIZE_ARG; i++) {
+        printf("argv4[%d]: %s ", i, argv4[i]);
+    }
+    printf("\n");
+    printf("argv5: ");
+    for (int i; i<MAX_SIZE_ARG; i++) {
+        printf("argv5[%d]: %s ", i, argv5[i]);
+    }
+    printf("\n");
+    */
 
-    //add pipe here later
+int check_pipe(){
+    int i = 0;
+    for (i=0; i < sizeofargv; i++){
+        if (!strcmp(argv[i], "|")) {
+            printf("pipe detected!!!\n");
+            return 1;
+        }
+    }
+    return 0;
+}
 
+void without_pipe() {
+    pid = fork();
+        if(pid < 0){
+            fprintf(stderr, "fork() Failed");
+            exit(1);
+        } else if (pid == 0) {
+            execvp(argv[0], argv);
+            printf("3230shell: '%s': No such file or directory\n", argv[0]);
+            exit(1);
+        } else {
+            if(NULL == argv[i]) {
+                waitpid(pid, NULL, 0);
+            }
+        }
+}
+
+void with_pipe() {
+    printf("work with pipe\n");
 }
